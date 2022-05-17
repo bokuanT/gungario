37525250b9ef57c22b970c4e89e8478e90ebb4ba
LAPTOP-ENK7NQAE\bokuan <bokuan2000@gmail.com>
2022-05-17 15:49:57 +0800

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+    public Animator animator;
+    public GameObject gameObject;
+
+    Vector2 movement;
+    Vector2 mousePos;
+    bool isRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Speed", Mathf.Abs(movement.x));
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
+        //Vector2 lookDir = mousePos - rb.position;
+        //float angle = Mathf.Atan2(lookDir.y ,lookDir.x) * Mathf.Rad2Deg - 90f;
+        //rb.rotation = angle;
+
+        if (movement.x > 0 && !isRight) {
+            FlipHorizontal();
+            isRight = true;
+        } else if (movement.x < 0 && isRight) {
+            FlipHorizontal();
+            isRight = false;
+        }
+    }
+
+    private void FlipHorizontal() {
+        Vector3 curScale = gameObject.transform.localScale;
+        curScale.x *= -1;
+        gameObject.transform.localScale = curScale;
+        isRight = !isRight;
+    }
+}
