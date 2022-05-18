2dd94ecc14399883bfc1c92f34c57645a372dd5b
ryanchua00 <ryanchuahj@gmail.com>
2022-05-18 13:56:38 +0800

    - Added temporary gun sprite, bullet sprite, explosion animation. - Added bullet logic on Collisions.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
+    }
+
+}
