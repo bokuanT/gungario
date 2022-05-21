2fca842c7a9339c30b0a9764589258996319f115
ryanchua00 <ryanchuahj@gmail.com>
2022-05-21 17:18:23 +0800

    The Multiplayer Update
    Changed:
    1. Imported Mono Cecil Package
    2. Imported Photon Fusion v1.0.0 Stable SDK
    
    Implemented:
    1. NetworkRunnerHandler -- Starts NetworkRunner PF (Prefab)
    2. NetworkRunner PF -- runs the Network and spawns the NetworkPlayer (Prefab)
    3. NetworkPlayer -- Player Prefab w/o camera
    
    Issues:
    1. Player spawns without a camera -- add to prefab
        - issue is evident through Scene view when running the game

---
+++ Assets/Network/NetworkRunnerHandler.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine.SceneManagement;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+public class NetworkRunnerHandler : MonoBehaviour
+{
+    public NetworkRunner networkRunnerPrefab;
+
+    NetworkRunner networkRunner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkRunner = Instantiate(networkRunnerPrefab);
+        networkRunner.name = "Network runner";
+
+        // AutoHostOrClient: First person starting the game is the host
+        // To be changed at a later date
+        var clientTask = InitializeNetworkRunner(networkRunner, GameMode.AutoHostOrClient, NetAddress.Any(), SceneManager.GetActiveScene().buildIndex, null);
+    }
+
+    protected virtual Task InitializeNetworkRunner(NetworkRunner runner, GameMode gameMode, NetAddress address, SceneRef scene, Action<NetworkRunner> initialized)
+    {
+        var sceneObjectProvider = runner.GetComponents(typeof(MonoBehaviour)).OfType<INetworkSceneObjectProvider>().FirstOrDefault();
+
+        if (sceneObjectProvider == null)
+        {
+            // Handle networked objects that already exit the scene
+            sceneObjectProvider = runner.gameObject.AddComponent<NetworkSceneManagerDefault>();
+        }
+
+        runner.ProvideInput = true;
+
+        return runner.StartGame(new StartGameArgs 
+        {
+            GameMode = gameMode,
+            Address = address,  
+            Scene = scene,
+            SessionName = "TestRoom",
+            Initialized = initialized,
+            SceneObjectProvider = sceneObjectProvider  
+        });
+    }
+
+}
---
+++ Assets/Network/Spawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System;
+
+public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
+{
+    public NetworkPlayer playerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        // on other player joined and current player is server 
+        if (runner.IsServer) 
+        {
+            Debug.Log("OnPlayerJoined we are server. Spawning player");
+            // function to spawn a player, with random position 
+            runner.Spawn(playerPrefab, Utils.GetRandomSpawnPoint(), Quaternion.identity, player);
+        }
+        Debug.Log("OnPlayerJoined");
+    }
+
+     public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnPlayerLeft(NetworkRunner runner,  PlayerRef player)
+    {
+        Debug.Log("OnPlayerLeft");
+    }
+
+        // handles successful connections to server/host
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        Debug.Log("OnConnectedToServer");
+    }
+    
+    public void OnShutdown(NetworkRunner runner, ShutdownReason reason) 
+    { 
+        Debug.Log("OnShutdown");
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] byteArr) 
+    {
+        Debug.Log("OnConnectRequest");
+    }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress address, NetConnectFailedReason reason) { Debug.Log("OnConnectFailed"); }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner) { Debug.Log("OnDisconnectedFromServer"); }
+
+
+    // These functions are just here to satisfy the Interface. 
+    // To be used in future updates perhaps
+   
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> list) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> dict) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken token) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> array) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+
+}
